06b391ccd829b605c4ad6cb4cbd11be51c7bf7f4
Jayanth Saikiran <jayanthsaikiran@gmail.com>
2021-02-12 01:15:54 +0000

    added binary search

---
+++ Search/binarysearch.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+// binary search
+int main() {
+  int n;
+  scanf("%d",&n);
+  int arr[n];
+
+  for(int i=0;i<n;i++){
+    int temp;
+    scanf("%d",&temp);
+    arr[i] = temp;
+  }
+}
