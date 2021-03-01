bab0b1065a0a124fbbe7c96ec74bb5dca325c987
Jayanth Saikiran <jayanthsaikiran@gmail.com>
2021-03-01 17:09:50 +0000

    added missing number code

--- Search/Missing and repeating number/MissingAndRepeating.cpp
+++ Search/Missing and repeating number/MissingAndRepeating.cpp
@@ -2,14 +2,18 @@
 using namespace std;
 
 void func(int arr[],int n){
-  for(int i=0;i<n;i++){
-    int temp = arr[abs(arr[i])];
-    if(temp < 0){
-      cout<<"Absolute: "<<abs(temp)<<endl;
-      break;
+    int sum=0,total = n * ((n+1)/2),rep = 0;
+    for(int i=0;i<n;i++){
+        sum += abs(arr[i]);
+        int temp = arr[abs(arr[i])];
+        if(temp < 0){
+            cout<<"Repeating: "<<abs(temp)<<endl;
+            rep = abs(temp);
+        }
+        arr[abs(arr[i])] = -arr[abs(arr[i])];
     }
-    arr[abs(arr[i])] = -arr[abs(arr[i])];
-  }
+    int missing = total-(sum-rep);
+    cout<<"Missing "<<missing<<endl;
 }
 
 int main() {
