1ad5a4d8c25709c3c631a196b8ab58638cc106b5
Jayanth Saikiran <jayanthsaikiran@gmail.com>
2022-05-22 13:55:14 +0000

    factors code

--- Basic Maths/Divisors/main.cpp
+++ Basic Maths/Divisors/main.cpp
@@ -1,7 +1,17 @@
 #include<iostream>
 using namespace std;
 
-void printDivisors(int n){}
+void printDivisors(int n){
+  for(int i=1;i*i<n;i++) {
+    if(n%i == 0){
+      cout<<i<<" ";
+
+      if( i != (n/i))
+        cout<<(n/i)<<" ";
+    }
+  }
+  
+}
 
 int main() {
   int n;
