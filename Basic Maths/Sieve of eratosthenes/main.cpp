3e18766035a61dda3b5ada6a6020120a18f6cb03
Jayanth Saikiran <jayanthsaikiran@gmail.com>
2022-05-23 11:13:30 +0000

    sieve of eratosthenes algo

--- Basic Maths/Sieve of eratosthenes/main.cpp
+++ Basic Maths/Sieve of eratosthenes/main.cpp
@@ -1,14 +1,23 @@
 #include<iostream>
+#include<limits.h>
 #include <vector>
 using namespace std;
 
 void sieveOfEratosthenes(int n){
   vector<bool> isPrime(n+1,true);
-
-  for(int i=2;i*i<n;i++){
+  cout<<endl;
+  for(int i=2;i*i<=n;i++) {
     if(isPrime[i]){
-      
+      for(int j=2*i;j<=n;j+=i){
+        isPrime[j] = false;
+      }
     }
+  }
+  
+  isPrime[1] = false;
+  for(int i=1;i<=n;i++) {
+    string result = isPrime[i] == 1?"Prime":"Not prime";
+    cout<<i<<" "<<result<<endl;
   }
 
 }
