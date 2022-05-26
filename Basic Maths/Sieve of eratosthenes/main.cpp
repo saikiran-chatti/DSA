a57eb4bfc9469c73cc392893edfec2b0d64fcd42
Jayanth Saikiran <jayanthsaikiran@gmail.com>
2022-05-26 16:37:17 +0000

    Sieve of eratosthenes

--- Basic Maths/Sieve of eratosthenes/main.cpp
+++ Basic Maths/Sieve of eratosthenes/main.cpp
@@ -1,30 +1,28 @@
-#include<iostream>
-#include<limits.h>
+#include <iostream>
+#include <limits.h>
 #include <vector>
 using namespace std;
 
-void sieveOfEratosthenes(int n){
-  vector<bool> isPrime(n+1,true);
-  cout<<endl;
-  for(int i=2;i*i<=n;i++) {
-    if(isPrime[i]){
-      for(int j=2*i;j<=n;j+=i){
-        isPrime[j] = false;
-      }
-    }
-  }
-  
-  isPrime[1] = false;
-  for(int i=1;i<=n;i++) {
-    string result = isPrime[i] == 1?"Prime":"Not prime";
-    cout<<i<<" "<<result<<endl;
-  }
+void sieveOfEratosthenes(int n) {
+	vector<bool> isPrime(n + 1, true);
+	for (int i = 2; i * i <= n; i++) {
+		if (isPrime[i]) {
+			for (int j = 2 * i; j <= n; j += i) {
+				isPrime[j] = false;
+			}
+		}
+	}
 
+	isPrime[1] = false;
+	for (int i = 1; i <= n; i++) {
+		string result = isPrime[i] == 1 ? "Prime" : "Not prime";
+		cout << i << " " << result << endl;
+	}
 }
 
 int main() {
-  int n;
-  cin>>n;
+	int n;
+	cin >> n;
 
-  sieveOfEratosthenes(n);
+	sieveOfEratosthenes(n);
 }
