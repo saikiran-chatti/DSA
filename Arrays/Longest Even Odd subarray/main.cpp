dd49ab546af05f6239e3eb77f4ec8a0702633b12
Jayanth Saikiran <jayanthsaikiran@gmail.com>
2022-07-28 18:30:31 +0000

    fixed longest even odd array bug

--- Arrays/Longest Even Odd subarray/main.cpp
+++ Arrays/Longest Even Odd subarray/main.cpp
@@ -4,12 +4,14 @@ using namespace std;
 int longestEvenOdd(int arr[],int n){
   int k = 1,res = 0;
   for(int i=1;i<n;i++){
-    if( (arr[i]%2 == 0 && arr[i-1]%2 == 1) || (arr[i]%2 == 1 && arr[i-1]%2 == 0) )
+    if( (arr[i]%2 == 0 && arr[i-1]%2 == 1) || (arr[i]%2 == 1 && arr[i-1]%2 == 0) ){
       k++;
+      res = max(k,res);
+    }
     else
       k = 1;
-    res = max(k,res);
   }
+  return res;
 }
 
 int main() {
--- Arrays/Max consecutive one in binary array/main.cpp
+++ Arrays/Max consecutive one in binary array/main.cpp
@@ -1,27 +1,43 @@
-#include<iostream>
-#include<cmath>
+#include <cmath>
+#include <iostream>
 
 using namespace std;
 
-int maxConsecutive1(int arr[],int n) {
-  int res =0,k=0;
-  for(int i=0;i<n;i++){
-    k = 0;
-    while(i<n && arr[i] == 1){
-      i++;
-      k++;
-    }
-    res = max(res,k);
-  }
-  return res;
+int maxConsecutive1(int arr[], int n) {
+	int res = 0, k = 0;
+	for (int i = 0; i < n; i++) {
+		k = 0;
+		while (i < n && arr[i] == 1) {
+			i++;
+			k++;
+		}
+		res = max(res, k);
+	}
+	return res;
+}
+
+int maxConsecutive2(int arr[], int n) {
+	int count = 0;
+	int result = 0;
+
+	for (int i = 0; i < n; i++) {
+		if (arr[i] == 0) {
+			count = 0;
+		} else {
+			count++;
+			result = max(result, count);
+		}
+	}
+
+	return result;
 }
 
 int main() {
-  int n;
-  cin>>n;
-  int arr[n];
-  for(int i=0;i<n;i++)
-    cin>>arr[i];
-  cout<<maxConsecutive1(arr,n);
-  return 0;
+	int n;
+	cin >> n;
+	int arr[n];
+	for (int i = 0; i < n; i++)
+		cin >> arr[i];
+	cout << maxConsecutive1(arr, n);
+	return 0;
 }
