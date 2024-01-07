aac38e00a43724fc98fcfbcccacb33ccdc56fc55
plainspoken-kiran <kiran@plainspoken.com>
2024-01-07 19:06:18 +0530

    added longest substring question

--- Educative/Longest Substring with k distinct/longest_substring.py
+++ Educative/Longest Substring with k distinct/longest_substring.py
@@ -1,15 +1,19 @@
 def longest_substring(nums,k):
-    start = 0
-    s = set()
+    s = {}
     res = 0
     j = 0
     for i in range(len(nums)):
-        s.add(nums[i])
-        if len(s) == k and j == 0:
-            j = i+1
-        if len(s) > k:
-            res = max(res,i-start)
-            start = j
-            s.clear()
+        if nums[i] not in s:
+            s[nums[i]] = 1
+        else:
+            s[nums[i]]+= 1
+        while len(s) > k and j < len(nums):
+            s[nums[j]]-=1
+            if s[nums[j]] == 0 :
+                del s[nums[j]] 
+            j += 1
+        res = max(res,i-j+1)
+    if res == 0: return -1
     return res
-print(longest_substring("aabbcc",3))
+
+print(longest_substring("aabacbebebe",3))
